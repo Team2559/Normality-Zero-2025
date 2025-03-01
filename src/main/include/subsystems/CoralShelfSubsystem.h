48c9f2ec94e73e4c97507a2e85555f7d572a4a41
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2025-03-01 11:51:41 -0500

    Fix include style for libraries

--- src/main/include/subsystems/CoralShelfSubsystem.h
+++ src/main/include/subsystems/CoralShelfSubsystem.h
@@ -2,9 +2,7 @@
 
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
-#include <frc/Servo.h>
-
-#include "rev/SparkMax.h"
+#include <rev/SparkMax.h>
 
 using namespace::rev::spark;
 
--- src/main/include/subsystems/CoralTroughSubsystem.h
+++ src/main/include/subsystems/CoralTroughSubsystem.h
@@ -3,8 +3,7 @@
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
 #include <frc/Servo.h>
-
-#include "rev/SparkMax.h"
+#include <rev/SparkMax.h>
 
 using namespace::rev::spark;
 
